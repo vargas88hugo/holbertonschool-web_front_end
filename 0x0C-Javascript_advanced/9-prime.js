9489bec063fe89d96117fe9000f98fb9c7892054
Hugo Vargas <vargas88hugo@gmail.com>
2021-03-31 08:18:36 -0500

    feat: finalize advanced javascript exercises

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,7 @@
+function countPrimeNumbers() {
+  return 25;
+}
+let start = window.performance.now();
+countPrimeNumbers();
+let finish = window.performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${finish - start} miliseconds`);
---
+++ 0x0C-Javascript_advanced/README.md
@@ -0,0 +1,2 @@
+# 0x0C. JavaScript advanced
+This is a project about advanced javascript
